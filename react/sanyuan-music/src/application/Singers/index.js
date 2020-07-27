28598a2eec265c1f9bc5d0587895f7d9edd39105
980739252 <980739252@qq.com>
2020-07-27 23:02:01 +0800

    27

--- react/sanyuan-music/src/application/Singers/index.js
+++ react/sanyuan-music/src/application/Singers/index.js
@@ -2,7 +2,9 @@ import React, { useEffect, useRef, memo } from 'react';
 import { connect } from 'react-redux';
 import {NavContainer} from './style'
 import {categoryTypes,alphaTypes} from '../../api/config';
+import Horizen from '../../baseUI/horizen-item/index';
 function Singers (props) {
+    const{ categroy,alpha}= props;
     const handleUpdateAlpha =()=>{
 
     }
@@ -12,20 +14,19 @@ function Singers (props) {
   return (
     <div>
         <NavContainer>
-             <Horizen title="分类(默认热门) ：" list={categoryTypes} handleClick={(v)=>handleUpdateCategory(v)}/>
-             <Horizen title="首字母" lsit={alphaTypes } handleClick={(v)=>handleUpdateAlpha(v)}/>
+             <Horizen title="分类(默认热门) ：" list={categoryTypes} handleClick={(v)=>handleUpdateCategory(v) }oldvalue={categroy}/>
+             <Horizen title="首字母" lsit={alphaTypes } handleClick={(v)=>handleUpdateAlpha(v)}oldvalue={alpha}/>
         </NavContainer>
     </div>
   )
 }
 
 const mapStateToProps = (state) => ({
-   return()
-
-})
-const matDispatchToProps = (dispatch) => {
-  return {
+  category:state.singers.category,
+  alpha: state.singers.alpha,
+});
+const matDispatchToProps = (dispatch) => ({
     
-  }
-}
-export default connect(mapStateTopProps, matDispatchToProps)(memo(Singers));
+      
+});
+export default connect(mapStateToProps, matDispatchToProps)(memo(Singers));
---
+++ react/sanyuan-music/src/baseUI/horizen-item/readme.md
@@ -0,0 +1,5 @@
+- 通用组件  horien-item
+字母索引  类型封装 
+list click  title
+- scroll  useEffect  难点
+    

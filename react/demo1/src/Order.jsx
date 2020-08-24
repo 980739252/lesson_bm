9d778ee0fef2caf14bb5a0563994e70c2a061e97
980739252 <980739252@qq.com>
2020-08-24 18:51:18 +0800

    8-24

---
+++ react/demo1/src/Order.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+export default class Order extends React.Component{
+    childRef=React.createRef()
+    parentRef=React.createRef()
+    child=()=>{
+        console.log(1)
+        // e.stopPropagation()
+    }
+    parent=()=>{
+        console.log(2)
+    }
+    componentDidMount(){
+        //自动判断哪一个Dom节点的回调该被执行
+        document.addEventListener('click',()=>{
+            console.log('document');
+        })
+
+        this.childRef.current.addEventListener('click',()=>{
+            console.log('child')
+        })
+        this.parentRef.current.addEventListener('click',()=>{
+            console.log('parent')
+        })
+    }
+    render(){
+        return(
+              <div onClick={this.parent} ref={this.parentRef}>
+            <div onClick={this.child} ref={this.childRef}>
+                child
+            </div>
+        </div>
+        )
+      
+    }
+}
